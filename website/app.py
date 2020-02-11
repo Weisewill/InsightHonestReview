c622ccee0bfcfc8f4c2b9a185780a8131fcd6468
Ubuntu <ubuntu@ip-10-0-0-10.us-west-2.compute.internal>
2020-02-11 03:28:32 +0000

    Update website

--- website/app.py
+++ website/app.py
@@ -53,6 +53,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 cur = conn.cursor()
 cur.execute("SELECT * FROM amazon_reviews LIMIT 3;")
 amazon_reviews = pd.read_sql_query("SELECT * FROM amazon_reviews LIMIT 3;", conn)
+reddit_comments = pd.read_sql_query("SELECT * FROM reddit_comments LIMIT 3;", conn)
 
 cur.execute("SELECT distinct(Name) FROM amazon_reviews ")
 name = cur.fetchall()
@@ -67,11 +68,12 @@ app.layout = html.Div([
 ])
 
 amazon_reviews = amazon_reviews[['name', 'review_body', 'word_count', 'score']]
+reddit_comments = reddit_comments[['name', 'body', 'word_count', 'score']]
 
 options = createDropdownOptions(name)
 
-dt_good = dash_table.DataTable(
-    id='datatable_good',
+amazon_dt_good = dash_table.DataTable(
+    id='amazon_datatable_good',
     style_data={
         'whiteSpace': 'normal',
         'height': 'auto'
@@ -80,8 +82,8 @@ dt_good = dash_table.DataTable(
     data=amazon_reviews.to_dict('records')
 )
 
-dt_bad = dash_table.DataTable(
-    id='datatable_bad',
+amazon_dt_bad = dash_table.DataTable(
+    id='amazon_datatable_bad',
     style_data={
         'whiteSpace': 'normal',
         'height': 'auto'
@@ -89,50 +91,89 @@ dt_bad = dash_table.DataTable(
     columns=[{"name":i, "id":i} for i in amazon_reviews.columns],
     data=amazon_reviews.to_dict('records')
 )
+
+reddit_dt_good = dash_table.DataTable(
+    id='reddit_datatable_good',
+    style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto'
+    },
+    columns=[{"name":i, "id":i} for i in reddit_comments.columns],
+    data=reddit_comments.to_dict('records')
+)
+
+reddit_dt_bad = dash_table.DataTable(
+    id='reddit_datatable_bad',
+    style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto'
+    },
+    columns=[{"name":i, "id":i} for i in reddit_comments.columns],
+    data=reddit_comments.to_dict('records')
+)
+
 
 
 app.layout = html.Div([
-    dcc.Markdown('''
-##### Enter video game:
-'''),
+    dcc.Markdown('''##### Enter video game:'''),
     dcc.Dropdown(
         id='reviews-dropdown',
         options=options,
-        value='Bloodborne'
+        value='minecraft'
     ),
     html.Div(id='dd-output-container'),
-    dcc.Markdown('''
-##### Enter min # of words for the review:
-'''),
-    dcc.Input(
-        id="word_count_input", type="number", placeholder="30"
-    ),
-    dcc.Markdown('''
-##### Best 3 reviews:
-'''),
-    dt_good,
-    dcc.Markdown('''
-##### Worst 3 reviews:
-'''),
-    dt_bad
+    dcc.Markdown('''##### Enter min # of words for the review:'''),
+    dcc.Input(id="word_count_input", type="number", placeholder="30"),
+    html.Div(id='amazon-output-1'),
+    html.Div(id='amazon-output-2'),
+    dcc.Markdown('''##### Best 3 reviews from Amazon:'''),
+    amazon_dt_good,
+    dcc.Markdown('''##### Worst 3 reviews from Amazon:'''),
+    amazon_dt_bad,
+    html.Div(id='reddit-output-1'),
+    html.Div(id='reddit-output-2'),
+    dcc.Markdown('''##### Best 3 reviews from Reddit:'''),
+    reddit_dt_good,
+    dcc.Markdown('''##### Worst 3 reviews from Reddit:'''),
+    reddit_dt_bad
 ])
 
 @app.callback(
     [Output(component_id='dd-output-container', component_property='children'),
-        Output('datatable_good', 'data'),
-        Output('datatable_bad', 'data')],
+        Output(component_id='amazon-output-1', component_property='children'),
+        Output(component_id='amazon-output-2', component_property='children'),
+        Output('amazon_datatable_good', 'data'),
+        Output('amazon_datatable_bad', 'data'),
+        Output(component_id='reddit-output-1', component_property='children'),
+        Output(component_id='reddit-output-2', component_property='children'),
+        Output('reddit_datatable_good', 'data'),
+        Output('reddit_datatable_bad', 'data')
+        ],
     [Input(component_id='reviews-dropdown', component_property='value'),
         Input(component_id='word_count_input', component_property='value')]
 )
 def updateTable(value, count):
     if count == None:
         count = 30
-    new_query_good = pd.read_sql_query("SELECT name, review_body, word_count, score FROM amazon_reviews WHERE name = '{}' and word_count >= {} and score > 0.5 ORDER BY score DESC LIMIT 3;".format(value, count), conn)
-    new_query_bad = pd.read_sql_query("SELECT name, review_body, word_count, score FROM amazon_reviews WHERE name = '{}' and word_count >= {} and score < -0.5 ORDER BY score ASC LIMIT 3;".format(value, count), conn)
+    amazon_query_good = pd.read_sql_query("SELECT name, review_body, word_count, score FROM amazon_reviews WHERE name = '{}' and word_count >= {} and score > 0.5 ORDER BY score DESC LIMIT 3;".format(value, count), conn)
+    amazon_query_bad = pd.read_sql_query("SELECT name, review_body, word_count, score FROM amazon_reviews WHERE name = '{}' and word_count >= {} and score < -0.5 ORDER BY score ASC LIMIT 3;".format(value, count), conn)
+    amazon_rev_all = pd.read_sql_query("SELECT COUNT(score) as all FROM amazon_reviews WHERE name = '{}'".format(value), conn)
+    amazon_rev_pos = pd.read_sql_query("SELECT COUNT(score) as pos FROM amazon_reviews WHERE name = '{}' and score >= 0".format(value), conn)   
+
+    reddit_query_good = pd.read_sql_query("SELECT name, body, word_count, score FROM reddit_comments WHERE name = '{}' and word_count >= {}  ORDER BY score DESC LIMIT 3;".format(value, count), conn)
+    reddit_query_bad = pd.read_sql_query("SELECT name, body, word_count, score FROM reddit_comments WHERE name = '{}' and word_count >= {} ORDER BY score ASC LIMIT 3;".format(value, count), conn)
+    reddit_rev_all = pd.read_sql_query("SELECT COUNT(score) as all FROM reddit_comments WHERE name = '{}'".format(value), conn)
+    reddit_rev_pos = pd.read_sql_query("SELECT COUNT(score) as pos FROM reddit_comments WHERE name = '{}' and score >= 0".format(value), conn)
 
     return ('You\'ve entered "{}"'.format(value), 
-        new_query_good.to_dict('records'),
-        new_query_bad.to_dict('records')
+            '{} reviews from Amazon.'.format(amazon_rev_all['all'][0]),
+            '{} reviews are positive. ({})%'.format(int(amazon_rev_pos['pos'][0]), 0 if amazon_rev_all['all'][0] == 0 else round(100*(float(amazon_rev_pos['pos'][0])/amazon_rev_all['all'][0] ), 0)),
+        amazon_query_good.to_dict('records'),
+        amazon_query_bad.to_dict('records'),
+            '{} reviews from Reddit.'.format(reddit_rev_all['all'][0]),
+            '{} reviews are positive. ({})%'.format(int(reddit_rev_pos['pos'][0]), 0 if reddit_rev_all['all'][0] == 0 else round(100*(float(reddit_rev_pos['pos'][0])/reddit_rev_all['all'][0] ), 0)),
+        reddit_query_good.to_dict('records'),
+        reddit_query_bad.to_dict('records')
         )
     
 if __name__ == '__main__':
