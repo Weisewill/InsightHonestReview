d9a5fdcd0ff5d59ba927bdb178ec3b84514cce1d
Weisewill <iamwtchiu@gmail.com>
2020-03-03 20:31:55 -0800

    Update prepareData.sh

--- spark/prepareData.sh
+++ spark/prepareData.sh
@@ -1,7 +1,2 @@
 rm -r /usr/local/spark/work/*
-spark-submit cleanData.py --start 2010 --end 2010 --type comments
-rm -r /usr/local/spark/work/*
-spark-submit cleanData.py --start 2011 --end 2011 --type comments
-rm -r /usr/local/spark/work/*
-spark-submit cleanData.py --start 2012 --end 2012 --type comments
-
+spark-submit cleanData.py --start 2006 --end 2019 --type comments
